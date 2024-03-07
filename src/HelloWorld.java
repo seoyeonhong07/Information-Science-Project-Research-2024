15bd8c69e92c4905354a0b2b08e3856c2d2b09ec
seoyeonone <seoyeon070826@naver.com>
2024-03-07 21:47:19 +0900

    v0.1 hello

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world ~");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello " + name + "!");
     }
 }
