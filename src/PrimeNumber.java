4132fc57f06896541f0a0f1fb7c9c50a087f1b36
seoyeonone <seoyeon070826@naver.com>
2024-03-14 19:22:08 +0900

    v.0.1 prime number

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -5,12 +5,13 @@ public class PrimeNumber {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         int count = 0;
-        for(int i=1; i<= number; i=i+1){
+        for(int i=2; i< number; i=i+1){  // -2 loop
             if(number % i == 0){
                 count = count +1;
             }
         }
-        if(count == 2)
+        //if(count == 2)
+        if(count == 0)
             System.out.println(number + " is a prime number~");
         else
             System.out.println(number + " is NOT prime number!");
