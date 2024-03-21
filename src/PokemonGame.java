11b8fe9c01f4d94260b2c6bdd243857644f7d800
seoyeonhong07 <seoyeonhong070826@gmail.com>
2024-03-21 19:20:11 +0900

    v0.1 pokemon game

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -1,18 +1,18 @@
-class Pokemon {
+class Pokemon{
     private String name;
     private int hp;
 
-    public Pokemon() {  // default constructor
+    public Pokemon() {
         System.out.println("default constructor!");
     }
 
-    public Pokemon(String name, int hp) {  // parameter constructor
-        this.name = name;
-        this.hp = hp;
+    public Pokemon(String name, int hp) {
+        name = name;  // remove this keyword
+        hp = hp;  // remove this keyword
         System.out.println("parameter constructor~");
     }
 
-    public System getName() {
+    public String getName() {
         return name;
     }
 
@@ -31,13 +31,13 @@ class Pokemon {
 
 public class PokemonGame {
     public static void main(String[] args) {
-        Pokemon p1 = new Pokemon("Squirtle", 70);  // parameter constructor
+        Pokemon p2 = new Pokemon("Squirtle", 70);
 
-        Pokemon p2 = new Pokemon();  // default constructor
-        // p1.hp = 50;  // hp has private access in Pokemon
+        Pokemon p1 = new Pokemon();
+        //p1.hp = 50;
         p1.setHp(50);
         p1.setName("Pikachu");
         System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
+        System.out.println(p2.getName() + "'s health is " + p2.getHp() + ".");  // null's health is 0.
     }
 }
-
