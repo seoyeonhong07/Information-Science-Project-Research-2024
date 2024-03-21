2ae2d705221a1d9755f8ac327050b74e7f782fc7
seoyeonhong07 <seoyeonhong070826@gmail.com>
2024-03-21 20:14:55 +0900

    v0.3 pokemon game

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -1,46 +1,4 @@
-class Pokemon{
-    //private String m_name;  // prefix
-    private String name;
-    private int hp;
-
-    public Pokemon() {
-        this.name = "nameless";
-        this.hp = 10;
-        System.out.println("default constructor!");
-    }
-
-    public Pokemon(String name, int hp) {  // rename parameter
-        //public Pokemon(String n, int h) {  // rename parameter
-        //name = name;  // remove this keyword
-        //hp = hp;  // remove this keyword
-
-        // The code below works fine. However, this code impairs readability.
-//        name = n;
-//        hp = h;
-
-        // m_name = name
-
-        this.name = name;
-        this.hp = hp;
-        System.out.println("parameter constructor~");
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getHp() {
-        return hp;
-    }
-
-    public void setHp(int hp) {
-        this.hp = hp;
-    }
-}
+import pokemons.Pokemon;
 
 public class PokemonGame {
     public static void main(String[] args) {
@@ -48,10 +6,9 @@ public class PokemonGame {
 
         Pokemon p1 = new Pokemon();
         System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
-        //p1.hp = 50;
         p1.setHp(50);
         p1.setName("Pikachu");
         System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
-        System.out.println(p2.getName() + "'s health is " + p2.getHp() + ".");  // null's health is 0.
+        System.out.println(p2.getName() + "'s health is " + p2.getHp() + ".");
     }
 }
---
+++ src/Pokemons/Pokemon.java
@@ -0,0 +1,34 @@
+package Pokemons;
+
+public class Pokemon{
+    private String name;
+    private int hp;
+
+    public Pokemon() {
+        this.name = "nameless";
+        this.hp = 10;
+        System.out.println("default constructor!");
+    }
+
+    public Pokemon(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+        System.out.println("parameter constructor~");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+}
