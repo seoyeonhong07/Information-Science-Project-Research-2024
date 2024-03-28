397748ab689b0ef1bd8a5f438e303053a55d14fc
seoyeonhong07 <seoyeonhong070826@gmail.com>
2024-03-28 21:35:31 +0900

    v1.7 pokemon game

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -2,7 +2,8 @@ import Pokemons.*;
 
 public class PokemonGame {
     public static void main(String[] args) {
-        Charizard c1 = new Charizard("Charizard", 150, new Wings());
+        Wings wings = new Wings();
+        Charizard c1 = new Charizard("Charizard", 150, wings());  //Aggregation
         Pikachu p1 = new Pikachu("Pikachu", 50, new Nofly());
 //        p1.setFlyable(new Nofly());
 //        c1.setFlyable(new Wings());
