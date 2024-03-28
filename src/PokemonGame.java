4dec88211de9e6743c3b2525217b72ca2f61332b
seoyeonhong07 <seoyeonhong070826@gmail.com>
2024-03-28 19:12:01 +0900

    v0.5 pokemon game

--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -1,14 +1,21 @@
-import pokemons.Pokemon;
+import Pokemons.Pikachu;
+import Pokemons.Pokemon;
 
 public class PokemonGame {
     public static void main(String[] args) {
-        Pokemon p2 = new Pokemon("Squirtle", 70);
+        Pikachu pika1 = new Pikachu("Pikachu", 50);
+        Pikachu pika2 = new Pikachu();
+        System.out.println(pika1.getName());
+        System.out.println(pika1.getHp());
 
-        Pokemon p1 = new Pokemon();
-        System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
-        p1.setHp(50);
-        p1.setName("Pikachu");
-        System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
-        System.out.println(p2.getName() + "'s health is " + p2.getHp() + ".");
+
+//        Pokemon p2 = new Pokemon("Squirtle", 70);
+//
+//        Pokemon p1 = new Pokemon();
+//        System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
+//        p1.setHp(50);
+//        p1.setName("Pikachu");
+//        System.out.println(p1.getName() + "'s health is " + p1.getHp() + ".");
+//        System.out.println(p2.getName() + "'s health is " + p2.getHp() + ".");
     }
 }
--- src/Pokemons/Pikachu.java
+++ src/Pokemons/Pikachu.java
@@ -1,4 +1,10 @@
 package Pokemons;
 
-public class Pikachu {
+public class Pikachu extends Pokemon {
+    public Pikachu() {
+    }
+
+    public Pikachu(String name, int hp){
+        super(name, hp);
+    }
 }
